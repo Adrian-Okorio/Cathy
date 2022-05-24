5ea959423a7e82aad83b1f3e26036abfc5a3a84c
Adrian <101798249+Aredo-A@users.noreply.github.com>
2022-05-24 20:25:50 +0300

    Update Cathy_Project.py

--- Cathy_Project.py
+++ Cathy_Project.py
@@ -4,5 +4,5 @@ import streamlit as st
 st.title("MAKERERE STUDENT’S ONLINE COMPUTER LAB SYSTEM")
 
 menu = ["Home", "Admin Login", "Students Login", "SignUp"]
- choice = st.sidebar.selectbox("Makerere University Computer Laboratory "
+    choice = st.sidebar.selectbox("Makerere University Computer Laboratory "
                                   " Management Sytem", menu)
